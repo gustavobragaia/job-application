2df22da64d756a4f6091ac9121a21140699abbcf
Gustavo Bragaia <gustavobragaia@Gustavos-MacBook-Pro.local>
2025-12-20 16:52:07 -0300

    feat: setup of nativewind and fix babel error for v4 [used stackoverflow]

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-2xl font-semibold text-blue-600">OK</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
